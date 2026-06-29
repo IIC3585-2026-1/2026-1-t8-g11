d41b4278991ddc4127bda945f6c5b0288a74dd7b
Jaime Pérez Lisboa <jaime.perez@uc.cl>
2026-06-28 22:32:21 -0400

    chore: configure chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 import RasterDisplay from "./components/RasterDisplay";
+import { Toolbar } from "./components/Toolbar";
+import { Provider } from "@/components/ui/provider";
 
 export default function App() {
-  return <RasterDisplay />;
+  return (
+    <Provider forcedTheme="light">
+      <Toolbar />
+      <RasterDisplay />
+    </Provider>
+  );
 }
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,9 @@
+import { RadioCard } from "@chakra-ui/react";
+
+export function Toolbar() {
+  return (
+    <RadioCard.Root>
+      <RadioCard.Label>Select Tool</RadioCard.Label>
+    </RadioCard.Root>
+  );
+}
